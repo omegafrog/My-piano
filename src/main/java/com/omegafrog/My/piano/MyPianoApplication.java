3a67c47166ebdd4a432e5a26f1024cbb7bc3ef28
omegafrog <jiwoo22110103@gmail.com>
2023-04-12 21:44:13 +0900

    Initial commit

---
+++ src/main/java/com/omegafrog/My/piano/MyPianoApplication.java
@@ -0,0 +1,13 @@
+package com.omegafrog.My.piano;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyPianoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyPianoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/omegafrog/My/piano/MyPianoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.omegafrog.My.piano;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyPianoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
