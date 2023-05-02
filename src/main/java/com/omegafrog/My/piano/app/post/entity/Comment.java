07527cea3a70e6dea8844d301709e9e08ef67886
omegafrog <jiwoo22110103@gmail.com>
2023-05-02 19:41:41 +0900

    feat : make post comment entity
    post에 달리는 comment eneity를 작성함.

---
+++ src/main/java/com/omegafrog/My/piano/app/post/entity/Comment.java
@@ -0,0 +1,31 @@
+package com.omegafrog.My.piano.app.post.entity;
+
+import javax.persistence.*;
+import javax.validation.constraints.NotNull;
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+public class Comment {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @OneToOne
+    @JoinColumn(name = "AUTHOR_ID")
+    private Author author;
+    @NotNull
+    private LocalDateTime createdAt;
+
+    @NotNull
+    private String content;
+
+    @NotNull
+    private int likeCount;
+
+    @OneToMany
+    private List<Comment> replies = new ArrayList<>();
+
+}
