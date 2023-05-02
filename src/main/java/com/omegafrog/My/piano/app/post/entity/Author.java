28ddd6a3606af507c0c1041545860653c01fae6d
omegafrog <jiwoo22110103@gmail.com>
2023-05-02 19:41:18 +0900

    feat : make post author VO
    post의 작성자에 해당하는 author의 VO를 생성함

---
+++ src/main/java/com/omegafrog/My/piano/app/post/entity/Author.java
@@ -0,0 +1,39 @@
+package com.omegafrog.My.piano.app.post.entity;
+
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+import javax.validation.constraints.NotNull;
+import java.util.Objects;
+
+@RequiredArgsConstructor
+@Getter
+public class Author {
+
+    @NotNull
+    private final Long id;
+
+    @NotNull
+    private final String name;
+    private final String profileSrc;
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        Author author = (Author) o;
+
+        if (!id.equals(author.id)) return false;
+        if (!name.equals(author.name)) return false;
+        return Objects.equals(profileSrc, author.profileSrc);
+    }
+
+    @Override
+    public int hashCode() {
+        int result = id.hashCode();
+        result = 31 * result + name.hashCode();
+        result = 31 * result + (profileSrc != null ? profileSrc.hashCode() : 0);
+        return result;
+    }
+}
