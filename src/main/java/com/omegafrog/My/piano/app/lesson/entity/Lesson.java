c03bee790fb8dd712e2edc6900a2e9585cb74eae
omegafrog <jiwoo22110103@gmail.com>
2023-05-05 11:02:28 +0900

    feat : make lesson entity
    lesson entity를 작성하고 artist, sheet의 연관관계를 설정함.
    Update를 위한 dto와 도메인 로직 추가함.

---
+++ src/main/java/com/omegafrog/My/piano/app/lesson/entity/Lesson.java
@@ -0,0 +1,77 @@
+package com.omegafrog.My.piano.app.lesson.entity;
+
+import com.omegafrog.My.piano.app.lesson.dto.UpdateLessonDto;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+import java.time.LocalDateTime;
+import java.time.LocalTime;
+import java.util.Objects;
+
+@Entity
+@NoArgsConstructor
+public class Lesson {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Getter
+    private Long id;
+
+    private String title;
+    private String subTitle;
+    private int price;
+
+    private VideoInformation videoInformation;
+
+    private LocalDateTime createdAt;
+    private int viewCount;
+    @OneToOne(cascade = {CascadeType.PERSIST, CascadeType.MERGE})
+    @JoinColumn(name = "ARTIST_ID")
+    private LessonProvider lessonProvider;
+    @OneToOne(cascade = {CascadeType.PERSIST, CascadeType.MERGE})
+    @JoinColumn(name = "SHEET_ID")
+    private Sheet sheet;
+
+    private LessonInformation lessonInformation;
+
+    @Builder
+    public Lesson(String title, String subTitle, int price, VideoInformation videoInformation,
+                  LessonProvider lessonProvider, Sheet sheet, LessonInformation lessonInformation) {
+        this.title = title;
+        this.subTitle = subTitle;
+        this.price = price;
+        this.videoInformation = videoInformation
+        this.lessonProvider = lessonProvider;
+        this.sheet = sheet;
+        this.lessonInformation = lessonInformation;
+        createdAt = LocalDateTime.now();
+        viewCount=0;
+    }
+
+    public Lesson update(UpdateLessonDto dto){
+        this.title = dto.getTitle();
+        this.subTitle = dto.getSubTitle();
+        this.price = dto.getPrice();
+        this.videoUrl = dto.getVideoUrl();
+        this.sheet = dto.getSheet();
+        this.lessonInformation = dto.getLessonInformation();
+        return this;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        Lesson lesson = (Lesson) o;
+
+        return Objects.equals(id, lesson.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return id != null ? id.hashCode() : 0;
+    }
+}
