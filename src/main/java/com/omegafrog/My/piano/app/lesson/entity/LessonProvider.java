a1ff32d87400a116d237b45a898b2bbf24e5b561
omegafrog <jiwoo22110103@gmail.com>
2023-05-05 11:02:28 +0900

    feat : make lesson entity
    lesson entity를 작성하고 artist, sheet의 연관관계를 설정함.
    Update를 위한 dto와 도메인 로직 추가함.

---
+++ src/main/java/com/omegafrog/My/piano/app/lesson/entity/LessonProvider.java
@@ -0,0 +1,43 @@
+package com.omegafrog.My.piano.app.lesson.entity;
+
+import lombok.Builder;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+@NoArgsConstructor
+public class LessonProvider {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+    private String profileSrc;
+
+
+    @Builder
+    public LessonProvider(String name, String profileSrc) {
+        this.name = name;
+        this.profileSrc = profileSrc;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        LessonProvider lessonProvider = (LessonProvider) o;
+
+        return id.equals(lessonProvider.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return id.hashCode();
+    }
+}
