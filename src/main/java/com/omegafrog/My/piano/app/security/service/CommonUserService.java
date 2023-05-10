4b925b7b4707d2b294c3576e0512a7ca99374c81
omegafrog <jiwoo22110103@gmail.com>
2023-05-10 15:50:49 +0900

    feat : UserDetails를 load하기 위한 commonUserService 작성
    loadUserByUsername : username으로 user를 조회
    registerUser : user 회원가입을 위한 메소드

--- src/main/java/com/omegafrog/My/piano/app/security/service/CommonUserService.java
+++ src/main/java/com/omegafrog/My/piano/app/security/service/CommonUserService.java
@@ -1,9 +1,12 @@
 package com.omegafrog.My.piano.app.security.service;
 
+import com.omegafrog.My.piano.app.dto.RegisterUserDto;
+import com.omegafrog.My.piano.app.dto.SecurityUserDto;
 import com.omegafrog.My.piano.app.security.entity.SecurityUser;
 import com.omegafrog.My.piano.app.security.entity.SecurityUserRepository;
+import com.omegafrog.My.piano.app.security.exception.UsernameAlreadyExistException;
+import com.omegafrog.My.piano.app.user.entity.User;
 import lombok.RequiredArgsConstructor;
-import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
@@ -21,6 +24,22 @@ public class CommonUserService implements UserDetailsService {
             return founded.get();
         }else{
             throw new UsernameNotFoundException("No such user.");
+        }
+    }
+
+    public SecurityUserDto registerUser(String username, String password, RegisterUserDto dto) throws UsernameAlreadyExistException {
+        User user = dto.toEntity();
+        SecurityUser securityUser = SecurityUser.builder()
+                .username(username)
+                .password(password)
+                .user(user)
+                .build();
+        Optional<SecurityUser> founded = securityUserRepository.findByUsername(securityUser.getUsername());
+        if (founded.isEmpty()) {
+            SecurityUser saved = securityUserRepository.save(securityUser);
+            return saved.toDto();
+        } else {
+            throw new UsernameAlreadyExistException("중복된 ID가 존재합니다.");
         }
     }
 }
