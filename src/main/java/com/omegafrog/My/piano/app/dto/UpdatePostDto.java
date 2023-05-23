3e17c523b2945936852c8efdce52e27f5efc1cd3
omegafrog <jiwoo22110103@gmail.com>
2023-05-23 23:46:22 +0900

    bug : controller의 binding을 위한 dto 의 noargsConstructor추가
    binding을 위해서는 getter, setter, noArgsConstructor가 필요하다. 따라서 이를 추가해야 정상적으로 binding할 수 있다.

--- src/main/java/com/omegafrog/My/piano/app/dto/UpdatePostDto.java
+++ src/main/java/com/omegafrog/My/piano/app/dto/UpdatePostDto.java
@@ -2,8 +2,10 @@ package com.omegafrog.My.piano.app.dto;
 
 import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
+@NoArgsConstructor
 public class UpdatePostDto {
     private String title;
     private String content;
