6ebb6556536a47893a3fbc5d1f9673341ac05c2a
omegafrog <jiwoo22110103@gmail.com>
2023-07-13 14:09:46 +0900

    feat : jpa repository 분리
    domain에 sheetPostRepository 를 구현하는 JPA 구체 클래스 작성.
    SimpleJpaSheetPostRepository : JPA 기본 리포지토리 빈 생성을 위한 인터페이스
    JpaSheetPostRepositoryImpl : domain의 sheetPostRepository를 구현하는 구체 클래스. 내부에서 SimpleJpaSheetPostRepository 사용.

--- src/main/java/com/omegafrog/My/piano/app/web/domain/sheet/SheetPostRepository.java
+++ src/main/java/com/omegafrog/My/piano/app/web/domain/sheet/SheetPostRepository.java
@@ -4,11 +4,13 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.Optional;
 
-public interface SheetPostRepository extends JpaRepository<SheetPost, Long> {
+public interface SheetPostRepository  {
 
     SheetPost save(SheetPost sheetPost);
 
     Optional<SheetPost> findById(Long id);
+
+    Optional<SheetPost> findBySheetId(Long sheetId);
 
     void deleteById(Long id);
 
