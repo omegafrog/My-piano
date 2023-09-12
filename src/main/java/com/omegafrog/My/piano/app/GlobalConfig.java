65e28614554cb66d1e32e684925c44455c7406ca
omegafrog <jiwoo22110103@gmail.com>
2023-09-12 20:28:09 +0900

    feat : aws client, template bean 생성

--- src/main/java/com/omegafrog/My/piano/app/GlobalConfig.java
+++ src/main/java/com/omegafrog/My/piano/app/GlobalConfig.java
@@ -10,17 +10,52 @@ import com.omegafrog.My.piano.app.web.service.LessonService;
 import com.omegafrog.My.piano.app.web.service.PostApplicationService;
 import com.omegafrog.My.piano.app.web.service.SheetPostApplicationService;
 import com.omegafrog.My.piano.app.web.service.VideoPostApplicationService;
+import io.awspring.cloud.s3.InMemoryBufferingS3OutputStreamProvider;
+import io.awspring.cloud.s3.Jackson2JsonS3ObjectConverter;
+import io.awspring.cloud.s3.S3Template;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import software.amazon.awssdk.auth.credentials.AwsBasicCredentials;
+import software.amazon.awssdk.auth.credentials.StaticCredentialsProvider;
+import software.amazon.awssdk.regions.Region;
+import software.amazon.awssdk.services.s3.S3Client;
+import software.amazon.awssdk.services.s3.presigner.S3Presigner;
 
 @Configuration
 public class GlobalConfig {
 
+    @Value("${spring.cloud.aws.credentials.access-key}")
+    private String accessToken;
+
+    @Value("${spring.cloud.aws.credentials.secret-key}")
+    private String secretToken;
+
+    @Value("${spring.cloud.aws.region.static}")
+    private String region;
+
+
     @Bean
-    public ObjectMapper objectMapper(){
+    public ObjectMapper objectMapper() {
         ObjectMapper objectMapper = new ObjectMapper();
         objectMapper.registerModule(new JavaTimeModule());
         return objectMapper;
+    }
+
+    @Bean
+    public S3Client s3Client() {
+        return S3Client.builder()
+                .region(Region.of(region))
+                .credentialsProvider(StaticCredentialsProvider.create(AwsBasicCredentials.create(
+                        accessToken, secretToken
+                )))
+                .build();
+    }
+
+    @Bean
+    public S3Template s3Template() {
+        return new S3Template(s3Client(), new InMemoryBufferingS3OutputStreamProvider(s3Client(), null),
+                new Jackson2JsonS3ObjectConverter(objectMapper()), S3Presigner.create());
     }
 
 }
