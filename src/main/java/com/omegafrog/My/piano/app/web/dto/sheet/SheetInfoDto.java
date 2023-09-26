240e2132cffa5c2690e17c6c19c827cb65fd1d79
omegafrog <jiwoo22110103@gmail.com>
2023-09-26 14:22:37 +0900

    feat : genre vo 추가

--- src/main/java/com/omegafrog/My/piano/app/web/dto/sheet/SheetInfoDto.java
+++ src/main/java/com/omegafrog/My/piano/app/web/dto/sheet/SheetInfoDto.java
@@ -1,7 +1,10 @@
 package com.omegafrog.My.piano.app.web.dto.sheet;
 
+import com.omegafrog.My.piano.app.web.domain.sheet.Genres;
 import com.omegafrog.My.piano.app.web.dto.order.SellableItemDto;
 import com.omegafrog.My.piano.app.web.dto.user.UserProfile;
+import com.omegafrog.My.piano.app.web.enums.Difficulty;
+import com.omegafrog.My.piano.app.web.enums.Instrument;
 import lombok.*;
 
 import java.time.LocalDateTime;
@@ -15,14 +18,26 @@ public class SheetInfoDto extends SellableItemDto {
     private String title;
     private LocalDateTime createdAt;
     private UserProfile artist;
+    private Genres genres;
+    private int pageNum;
+    private Difficulty difficulty;
+    private Instrument instrument;
+    private boolean isSolo;
+    private boolean lyrics;
     private String sheetUrl;
 
     @Builder
-    public SheetInfoDto(Long id, String title, LocalDateTime createdAt, UserProfile artist, String sheetUrl) {
+    public SheetInfoDto(Long id, String title, LocalDateTime createdAt, UserProfile artist, Genres genres, int pageNum, Difficulty difficulty, Instrument instrument, boolean isSolo, boolean lyrics, String sheetUrl) {
         this.id = id;
         this.title = title;
         this.createdAt = createdAt;
         this.artist = artist;
+        this.genres = genres;
+        this.pageNum = pageNum;
+        this.difficulty = difficulty;
+        this.instrument = instrument;
+        this.isSolo = isSolo;
+        this.lyrics = lyrics;
         this.sheetUrl = sheetUrl;
     }
 }
