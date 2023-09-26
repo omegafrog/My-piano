cb05ef17094053720035dc8efc39a14e7a89c370
omegafrog <jiwoo22110103@gmail.com>
2023-09-26 14:21:40 +0900

    feat : pid file 생성

--- src/main/java/com/omegafrog/My/piano/MyPianoApplication.java
+++ src/main/java/com/omegafrog/My/piano/MyPianoApplication.java
@@ -2,12 +2,15 @@ package com.omegafrog.My.piano;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.ApplicationPidFileWriter;
 
 @SpringBootApplication
 public class MyPianoApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(MyPianoApplication.class, args);
+		SpringApplication app = new SpringApplication(MyPianoApplication.class);
+		app.addListeners(new ApplicationPidFileWriter()); // ApplicationPidFileWriter 설정
+		app.run(args);
 	}
 
 }
