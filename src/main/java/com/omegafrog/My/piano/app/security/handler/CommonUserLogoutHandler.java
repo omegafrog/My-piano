391caec2096d3f288ba1fb2e910c6e0c62c096fb
omegafrog <jiwoo22110103@gmail.com>
2023-10-09 11:48:37 +0900

    feat : logout관련 설정
    logoutHandler에 transaction 설정

--- src/main/java/com/omegafrog/My/piano/app/security/handler/CommonUserLogoutHandler.java
+++ src/main/java/com/omegafrog/My/piano/app/security/handler/CommonUserLogoutHandler.java
@@ -13,6 +13,7 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpStatus;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.web.authentication.logout.LogoutHandler;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.io.IOException;
 
@@ -27,6 +28,7 @@ public class CommonUserLogoutHandler implements LogoutHandler {
     private String secret;
 
     @Override
+    @Transactional
     public void logout(HttpServletRequest request, HttpServletResponse response, Authentication authentication) {
         // refreshTokenRepository에서 로그아웃한 유저의 refresh token을 삭제하면 됨.
         String accessToken = TokenUtils.getAccessTokenStringFromHeaders(request);
