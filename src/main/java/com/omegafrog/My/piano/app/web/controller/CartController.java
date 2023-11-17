2cacf603ccac4fa8978dee5bee653f02f4f92e59
omegafrog <jiwoo22110103@gmail.com>
2023-11-17 16:30:51 +0900

    feat : cart에 상품 추가하는 기능 추가

--- src/main/java/com/omegafrog/My/piano/app/web/controller/CartController.java
+++ src/main/java/com/omegafrog/My/piano/app/web/controller/CartController.java
@@ -3,16 +3,15 @@ package com.omegafrog.My.piano.app.web.controller;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.omegafrog.My.piano.app.utils.AuthenticationUtil;
+import com.omegafrog.My.piano.app.web.domain.order.SellableItemFactory;
 import com.omegafrog.My.piano.app.web.domain.user.User;
 import com.omegafrog.My.piano.app.web.dto.order.OrderDto;
 import com.omegafrog.My.piano.app.web.dto.order.OrderRegisterDto;
-import com.omegafrog.My.piano.app.utils.exception.payment.PaymentException;
 import com.omegafrog.My.piano.app.web.service.CartApplicationService;
 import com.omegafrog.My.piano.app.web.service.OrderService;
 import com.omegafrog.My.piano.app.utils.response.APISuccessResponse;
 import com.omegafrog.My.piano.app.utils.response.JsonAPIResponse;
 import com.omegafrog.My.piano.app.utils.response.ResponseUtil;
-import jakarta.persistence.EntityNotFoundException;
 import jakarta.persistence.PersistenceException;
 import jakarta.servlet.http.HttpServletRequest;
 import lombok.RequiredArgsConstructor;
@@ -33,8 +32,18 @@ public class CartController {
     private ObjectMapper objectMapper;
 
     private final OrderService orderService;
+    @Autowired
+    private final SellableItemFactory sellableItemFactory;
 
-
+    @PostMapping("{mainResource}")
+    public JsonAPIResponse addToCart(@RequestBody OrderRegisterDto dto, @PathVariable String mainResource, HttpServletRequest request)
+            throws JsonProcessingException {
+        User loggedInUser = AuthenticationUtil.getLoggedInUser();
+        OrderDto createdOrder = orderService.makeOrder(mainResource, dto);
+        List<OrderDto> orderDtos = cartService.addToCart(createdOrder, loggedInUser);
+        Map<String, Object> data = ResponseUtil.getStringObjectMap("contents", orderDtos);
+        return new APISuccessResponse("Add order to cart success.", data);
+    }
 
     @DeleteMapping("/{id}")
     public JsonAPIResponse deleteFromCart(@PathVariable Long id)
@@ -44,12 +53,6 @@ public class CartController {
         return new APISuccessResponse("Delete order from cart success.");
     }
 
-    @GetMapping("/pay")
-    public JsonAPIResponse payCart() throws PaymentException {
-        User loggedInUser = AuthenticationUtil.getLoggedInUser();
-        cartService.payAll(loggedInUser);
-        return new APISuccessResponse("Buy items in your cart success.");
-    }
 
     @GetMapping("")
     public JsonAPIResponse getAllContentFromCart() throws JsonProcessingException , PersistenceException {
