5f15406d89c3c10d89081b13788f382eec5724ae
omegafrog <jiwoo22110103@gmail.com>
2024-03-25 23:18:18 +0900

    codedeploy 설정 파일 추가와  helthCheck용 SystemController 추가

--- src/main/java/com/omegafrog/My/piano/app/web/domain/notification/PushInstance.java
+++ src/main/java/com/omegafrog/My/piano/app/web/domain/notification/PushInstance.java
@@ -7,21 +7,23 @@ import com.google.firebase.FirebaseOptions;
 import com.google.firebase.messaging.FirebaseMessaging;
 import com.google.firebase.messaging.FirebaseMessagingException;
 import com.google.firebase.messaging.Message;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.IOException;
 
-@Component
+@Slf4j
 public class PushInstance {
-
 
     private FirebaseApp app;
 
-    public PushInstance() throws IOException {
+    public PushInstance(String serviceAccountPath) throws IOException {
+        log.info("{}", serviceAccountPath);
         FileInputStream serviceAccount =
-                new FileInputStream("src/main/resources/firebase-admin.json");
+                new FileInputStream(serviceAccountPath);
 
         FirebaseOptions options = new FirebaseOptions.Builder()
                 .setCredentials(GoogleCredentials.fromStream(serviceAccount))
