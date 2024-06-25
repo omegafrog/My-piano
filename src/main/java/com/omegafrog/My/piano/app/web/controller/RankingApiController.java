31c2ed18953552398830be0f410a54df4f9c6658
omegafrog <jiwoo22110103@gmail.com>
2024-06-25 22:54:49 +0900

    refactor : refactor ranking api controller
    authenticationUtil service layer 안으로 변경.
    
    controller parameter validation

--- src/main/java/com/omegafrog/My/piano/app/web/controller/RankingApiController.java
+++ src/main/java/com/omegafrog/My/piano/app/web/controller/RankingApiController.java
@@ -6,6 +6,8 @@ import com.omegafrog.My.piano.app.web.response.ResponseUtil;
 import com.omegafrog.My.piano.app.web.domain.ranking.PopularRankingItem;
 import com.omegafrog.My.piano.app.web.enums.DateRangeType;
 import com.omegafrog.My.piano.app.web.service.ranking.RankingApiApplicationService;
+import jakarta.validation.Valid;
+import jakarta.validation.constraints.NotNull;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -25,11 +27,12 @@ public class RankingApiController {
     private RankingApiApplicationService rankingService;
 
     @GetMapping("popular")
-    public JsonAPISuccessResponse getPopularSheetPost(@RequestParam DateRangeType range, @RequestParam String limit)
+    public JsonAPISuccessResponse getPopularSheetPost(
+            @Valid @NotNull @RequestParam DateRangeType range,
+            @Valid @NotNull @RequestParam String limit)
             throws IOException, TimeoutException {
         List<PopularRankingItem> popularSheetPost = rankingService.getPopularSheetPost(range, limit);
-        Map<String, Object> data = ResponseUtil.getStringObjectMap("popular", popularSheetPost);
-        return new ApiSuccessResponse("Get popular sheet post success.", data);
+        return new ApiSuccessResponse("Get popular sheet post success.",popularSheetPost);
     }
 
 }
