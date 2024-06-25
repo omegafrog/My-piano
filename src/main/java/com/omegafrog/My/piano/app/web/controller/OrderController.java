15dd297b51e0defc443a81d85e397128f88bc339
omegafrog <jiwoo22110103@gmail.com>
2024-06-25 22:42:12 +0900

    refactor : order controller parameter validation

--- src/main/java/com/omegafrog/My/piano/app/web/controller/OrderController.java
+++ src/main/java/com/omegafrog/My/piano/app/web/controller/OrderController.java
@@ -5,6 +5,8 @@ import com.omegafrog.My.piano.app.web.dto.order.OrderRegisterDto;
 import com.omegafrog.My.piano.app.web.response.success.ApiSuccessResponse;
 import com.omegafrog.My.piano.app.web.response.success.JsonAPISuccessResponse;
 import com.omegafrog.My.piano.app.web.service.OrderService;
+import jakarta.validation.Valid;
+import jakarta.validation.constraints.NotNull;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -19,26 +21,28 @@ import java.util.List;
 @RequestMapping("/api/v1")
 public class OrderController {
 
-    @Autowired
     private final OrderService orderService;
 
     @PostMapping("/order/{mainResource}")
     public JsonAPISuccessResponse orderItem(
-            @PathVariable String mainResource,
-            @Validated @RequestBody OrderRegisterDto order) {
+            @Valid @NotNull @PathVariable String mainResource,
+            @Valid @NotNull @RequestBody OrderRegisterDto order) {
         OrderDto createdOrder = orderService.makeOrder(mainResource, order);
         OrderDto processedOrder = orderService.makePayment(createdOrder);
         return new ApiSuccessResponse("Buy " + mainResource + " success.", processedOrder);
     }
 
     @GetMapping("/order/{mainResource}/{id}")
-    public JsonAPISuccessResponse isOrderedItem(@PathVariable String mainResource, @PathVariable Long id){
+    public JsonAPISuccessResponse isOrderedItem(
+            @Valid @NotNull @PathVariable String mainResource,
+            @Valid @NotNull @PathVariable Long id){
         boolean isOrdered = orderService.isOrderedItem(mainResource, id);
         return new ApiSuccessResponse("Check isOrdered " + mainResource + "success.", isOrdered);
     }
 
     @GetMapping(path = "/order/{id}/cancel")
-    public JsonAPISuccessResponse cancelOrder(@PathVariable Long id) {
+    public JsonAPISuccessResponse cancelOrder(
+            @Valid @NotNull @PathVariable Long id) {
         orderService.deleteOrder(id);
         return new ApiSuccessResponse("Cancel order success.");
     }
